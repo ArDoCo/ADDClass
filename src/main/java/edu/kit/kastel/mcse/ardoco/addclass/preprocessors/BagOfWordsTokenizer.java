b2b7367fa676d9a10878619c6660510c81531f3b
Jan Keim <jan.keim@kit.edu>
2022-01-26 10:32:23 +0100

    add classes that should be implemented soon

---
+++ src/main/java/edu/kit/kastel/mcse/ardoco/addclass/preprocessors/BagOfWordsTokenizer.java
@@ -0,0 +1,10 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.addclass.preprocessors;
+
+/**
+ * @author Jan Keim
+ *
+ */
+public class BagOfWordsTokenizer implements Tokenizer {
+    // TODO
+}
---
+++ src/main/java/edu/kit/kastel/mcse/ardoco/addclass/preprocessors/Tokenizer.java
@@ -0,0 +1,10 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.addclass.preprocessors;
+
+/**
+ * @author Jan Keim
+ *
+ */
+public interface Tokenizer {
+    // TODO
+}
